322245c7098e081011d3dc979212434659550da3
Hyun Kiwon <rhrnak2304@naver.com>
2023-08-08 11:53:46 +0900

    update

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -18,27 +18,59 @@ export const home = async (req, res) => {
   //   return res.render("server-error", { error });
   // }
   const videos = await Video.find({});
-  console.log(videos);
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
+  // findById : 해당 id로 해당 영상을 찾을 수 있다.
+  const videos = await Video.findById(id);
 
-  res.render("watch", {
-    pageTitle: `Watching
-   `,
-  });
+  if (videos) {
+    return res.render("watch", {
+      pageTitle: videos.title,
+      videos,
+    });
+  }
+  return res.render("404", { pageTitle: "Video not found." });
 };
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-
-  res.render("edit", { pageTitle: `Editing` });
+  const videos = await Video.findById(id);
+  console.log(videos);
+  if (videos) {
+    return res.render("edit", {
+      pageTitle: `Editing: ${videos.title}`,
+      videos,
+    });
+  }
+  return res.render("404", { pageTitle: "Video not found." });
 };
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
-
-  return res.redirect(`/videos/${id}`);
+  const { title, description, hashtags } = req.body;
+  // 해당 영상을 찾는 방법 원하는거 써라
+  const videos = await Video.findById(id);
+  // const videos = await Video.exists({ _id: id });
+  if (videos) {
+    // 축약형(훨 씐 좋다)
+    // findByIdAndUpdate(id, 변경내용) : 해당 아이디를 업데이트 해준다.
+    await Video.findByIdAndUpdate(id, {
+      title,
+      description,
+      haghtags: hashtags
+        .split(",")
+        .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+    });
+    // videos.title = title;
+    // videos.description = description;
+    // // #중복 체크
+    // videos.hashtags = hashtags
+    //   .split(",")
+    //   .map((word) => (word.startsWith("#") ? word : `#${word}`));
+    // await videos.save();
+    return res.redirect(`/videos/${id}`);
+  }
+  return res.render("404", { pageTitle: "Video not found." });
 };
 export const search = (req, res) => {
   res.send("Search");
@@ -50,35 +82,41 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 export const postUpload = async (req, res) => {
-  const { title, description, hashtags } = req.body;
-  // mongoose는 유효성 검사를 해준다.
-  // 영상을 만들기 위해서는 document를 만들어야 한다.
-  // 방법 1. 객체를 만든다
-  // const video = new Video({
-  //   title: title,
-  //   description,
-  //   createdAt: Date.now(),
-  //   // 문자입력시 앞에 #이 붙게 함
-  //   hashtags: hashtags.split(",").map((word) => `#${word}`),
-  //   meta: {
-  //     views: 0,
-  //     rating: 0,
-  //   },
-  // });
-  // 이제 비디오를 저장하고 db에 넘겨줘야 한다.
-  // 저장하고 넘겨주는데 시간이 걸리기 때문에 await를 걸어준다.
-  // await video.save();
-  // 방법 2.
-  await Video.create({
-    title: title,
-    description,
-    createdAt: Date.now(),
-    // 문자입력시 앞에 #이 붙게 함
-    hashtags: hashtags.split(",").map((word) => `#${word}`),
-    meta: {
-      views: 0,
-      rating: 0,
-    },
-  });
-  return res.redirect("/");
+  try {
+    const { title, description, hashtags } = req.body;
+    // mongoose는 유효성 검사를 해준다.
+    // 영상을 만들기 위해서는 document를 만들어야 한다.
+    // 방법 1. 객체를 만든다
+    // const video = new Video({
+    //   title: title,
+    //   description,
+    //   createdAt: Date.now(),
+    //   // 문자입력시 앞에 #이 붙게 함
+    //   hashtags: hashtags.split(",").map((word) => `#${word}`),
+    //   meta: {
+    //     views: 0,
+    //     rating: 0,
+    //   },
+    // });
+    // 이제 비디오를 저장하고 db에 넘겨줘야 한다.
+    // 저장하고 넘겨주는데 시간이 걸리기 때문에 await를 걸어준다.
+    // await video.save();
+    // 방법 2.
+    await Video.create({
+      title: title,
+      description,
+
+      // 문자입력시 앞에 #이 붙게 함
+      hashtags: hashtags
+        .split(",")
+        .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    // 에러 발생시 홈을 다시 가지않고 계속 머무른다.
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
--- src/models/Video.js
+++ src/models/Video.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 
-// db에 들어갈 형식을 만든다.
+// db에 들어갈 형식(모델)을 만든다.
 const videoSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  createdAt: Date,
-  hashtags: [{ type: String }],
+  title: { type: String, required: true, trim: true, maxlength: 20 },
+  description: { type: String, required: true, trim: true, minlength: 5 },
+  // Date.now()     ()가 들어간 함수식은 실행 하면 바로 실행이 된다. 그래서 () 제거해서 submit이 될때만 실행하게 한다.
+  createdAt: { type: Date, required: true, default: Date.now },
+  hashtags: [{ type: String, trim: true }],
   meta: {
-    views: Number,
-    rating: Number,
+    views: { type: Number, default: 0, required: true },
+    rating: { type: Number, default: 0, required: true },
   },
 });
 
