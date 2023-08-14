cc2b39b36c1945198fdc52e5c0c80b6bd322981e
Hyun Kiwon <rhrnak2304@naver.com>
2023-08-14 13:18:32 +0900

    update

--- src/controllers/연습폴더/userController _githubLogin.js
+++ src/controllers/연습폴더/userController _githubLogin.js
@@ -1,5 +1,6 @@
-import User from "../models/User";
+import User from "../../models/User";
 import bcrypt from "bcrypt";
+import { response } from "express";
 import fetch from "node-fetch";
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
@@ -7,9 +8,14 @@ export const getJoin = (req, res) => {
 
 export const postJoin = async (req, res) => {
   const { name, username, email, password, password2, location } = req.body;
+  // 중복체크는 exists로,  두개를 or 체크
   const exists = await User.exists({ $or: [{ username }, { email }] });
+  // const usernameExits = await User.find({ username });
   const pageTitle = "Join";
+  // 비번확인
   if (password !== password2) {
+    // 브라우저는 코드200을 준다 <=== 문제가 없다
+    // 하지만 우리가 직접 문제가 있다고 400을줘야 한다.
     return res.status(400).render("Join", {
       pageTitle,
       errorMessage: "password confirm not match",
@@ -21,6 +27,14 @@ export const postJoin = async (req, res) => {
       errorMessage: "This username/email is already taken",
     });
   }
+  // 두개의 경우의 수는 코드 수가 많아지니 위의 존재 여부를 간결하게 확인
+  // const emailExists = await User.exists({ email });
+  // if (emailExists) {
+  //   return res.render("Join", {
+  //     pageTitle,
+  //     errorMessage: "This email is already taken",
+  //   });
+  // }
   try {
     await User.create({
       name,
@@ -44,25 +58,38 @@ export const getLogin = (req, res) => {
   res.render("login", { pageTitle: "Login" });
 };
 export const postLogin = async (req, res) => {
+  // 계정 존재 확인
   const { username, password } = req.body;
   const pageTitle = "Login";
+  // find : 전체나 여러개를 보고 싶을때
+  // findOne : 해당하는 한가지만
   const user = await User.findOne({ username, socialOnly: false });
+  // const exists = await User.exists({ username });
   if (!user) {
     return res
       .status(400)
       .render("login", { pageTitle, errorMessage: "no exists" });
   }
+  // bcrypt.compare : 해시값 비교가능
   const ok = await bcrypt.compare(password, user.password);
   if (!ok) {
     return res
       .status(400)
       .render("login", { pageTitle, errorMessage: "Wrong pwd!!!" });
   }
+  // 로그인하면 세션을 준다.
+  // userd대한 정보를 저장한다.
+  // 이제 이정보를 pug로 줘야함
+  // locals이란 것은 express가 pug에서 그냥 사용할 수 있게 해준다.
+  // 즉 pug에서 locals object에 그냥 접근 가능(변수명으로 그냥 접근 가능)
+
+  // session 초기화부분
   req.session.loggedIn = true;
   req.session.user = user;
 
   return res.redirect("/");
 };
+// 유저를 github 로그인사이트로 보내주는 주소 작업 중
 export const startGithubLogin = (req, res) => {
   const baseUrl = `https://github.com/login/oauth/authorize`;
   const config = {
@@ -70,10 +97,12 @@ export const startGithubLogin = (req, res) => {
     allow_signup: false,
     scope: "read_user user:email",
   };
+  // new URLSearchParams().toString() : 객체안의 값들을 주소처럼 인코딩해준다.
   const params = new URLSearchParams(config).toString();
   const finalUrl = `${baseUrl}?${params}`;
   return res.redirect(finalUrl);
 };
+// 내가 설정한 github사이트로 이동시켜준다.
 export const finishGithubLogin = async (req, res) => {
   const baseUrl = "https://github.com/login/oauth/access_token";
   const config = {
@@ -83,6 +112,8 @@ export const finishGithubLogin = async (req, res) => {
   };
   const params = new URLSearchParams(config).toString();
   const finalUrl = `${baseUrl}?${params}`;
+  // fetch는 브라우저만 돌아가고 node.js에선 작동안해서 fetch설치해야함 버전은 2.6.1
+
   const tokenRequest = await (
     await fetch(finalUrl, {
       method: "POST",
@@ -91,6 +122,7 @@ export const finishGithubLogin = async (req, res) => {
       },
     })
   ).json();
+  // 토큰으로 접근한다.
   if ("access_token" in tokenRequest) {
     const { access_token } = tokenRequest;
     const apiUrl = "https://api.github.com";
@@ -116,7 +148,10 @@ export const finishGithubLogin = async (req, res) => {
     if (!emailObject) {
       return res.redirect("/login");
     }
+    // const test = await User.findOne({ e: emailObject.email });
+    // console.log(test);
     let user = await User.findOne({ email: emailObject.email });
+    // console.log(existingUser);
     if (!user) {
       user = await User.create({
         avatarUrl: userData.avatar_url,
@@ -137,6 +172,7 @@ export const finishGithubLogin = async (req, res) => {
 };
 
 export const logout = (req, res) => {
+  // 세션을 파괴해서 로그아웃
   req.session.destroy();
   return res.redirect("/");
 };
--- src/server.js
+++ src/server.js
@@ -15,48 +15,16 @@ const morganMiddle = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 
-// 세션설정
-// 사이트로 들어오는 모든것을 기억
-// express가 알아서 브라우저를 위한 id를 만들고 준다.
-// 그리고 브라우저가 id를 저장하여 기억
-// 즉 유저에게 쿠키를줘서 방문할때마다 백엔드에서 쿠키를보고 판별
 app.use(
   session({
-    // 초기 설정을 할 수 있다.
-    // session의 이름?? 이름을 타고 다른 사람이 나인척 할 수 있는것을 방지
     secret: process.env.COOKIE_SECRET,
-    // 쿠키 생존 시간 설정
-    // cookie: {
-    //   maxAge: 20000,
-    // },
-    // 세션이 수정이 될 때만 db에 저장해라(true : 무조건 다 저장, fasle: 수정될때만)
-    // 둘 다 비슷한 말이다, 보통 절약을 위해 false설정
     resave: false,
     saveUninitialized: false,
-    // session의 데이터들을 db에 저장
-    // session은 우리의 브라우저가 만들어질때 자동 생성
-    // 데이터가 db에 저장되서 다시 들어와도 유지된다.
     store: MongoStore.create({
       mongoUrl: process.env.DB_URL,
     }),
   })
 );
-// 백엔드에서 어떤 쿠키를 받는지 보기위한 것
-// app.use((req, res, next) => {
-//   res.locals.변수명 = 변수;
-//   res.locals.sexy = "you";
-//   res.locals.sitename = "hello youtube";
-//   req.sessionStore.all((error, sessions) => {
-//     console.log(sessions);
-//     next();
-//   });
-// });
-
-// app.get("/add-one", (req, res, next) => {
-//   req.session.potato += 1;
-//   return res.send(`${req.session.id}\n${req.session.potato}`);
-// });
-// ===============================
 app.use(localsMiddleware);
 app.use(morganMiddle);
 app.use(express.urlencoded({ extended: true }));
